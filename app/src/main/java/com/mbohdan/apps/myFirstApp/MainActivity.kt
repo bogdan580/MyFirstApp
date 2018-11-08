81eda71661417be1c3161f540968d8f47c965086
Nickolay <bogdan.nick007@gmail.com>
2018-11-08 07:25:00 +0100

    Added camera scanner QR

--- app/src/main/java/com/mbohdan/apps/myFirstApp/MainActivity.kt
+++ app/src/main/java/com/mbohdan/apps/myFirstApp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.mbohdan.apps.myFirstApp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,8 +19,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var mapFragment: SupportMapFragment
+    private var btn: Button? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -26,6 +30,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        result = findViewById(R.id.tvresult) as TextView
+
+        btn = findViewById(R.id.btn) as Button
+
+        btn!!.setOnClickListener {
+            val intent = Intent(this@MainActivity, ScanActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -34,19 +47,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 this.setTitle(R.string.title_home)
                     mapFragment.onResume()
                     mapView.visibility = View.VISIBLE
-
+                    scanner.visibility = View.INVISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 this.setTitle(R.string.title_scanner)
                     mapFragment.onPause()
                     mapView.visibility = View.INVISIBLE
+                    scanner.visibility = View.VISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
                 this.setTitle(R.string.title_notifications)
                     mapFragment.onPause()
                     mapView.visibility = View.INVISIBLE
+                    scanner.visibility = View.INVISIBLE
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -65,4 +80,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(echo).title("Marker in ECHO"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(psk))
     }
+
+    companion object {
+        var result: TextView? = null
+    }
+
 }
